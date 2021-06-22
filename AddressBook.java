bf054edb94891430db51bed0e655e5db98fcd365
balu-pinisety <manikantapandu225@gmail.com>
2021-06-23 04:45:22 +0530

    Added: AddressBook.java file with Welocme Message in it

---
+++ AddressBook.java
@@ -0,0 +1,14 @@
+package com.AddressBook;
+
+/**
+ * @author BALU
+ * Creating an Address Book of Persons details for user and can Add, Delete, Edit and Show it
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO display welcome message
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
